1987b60efc3c107e059493efaa4ffaaa3a29a687
rzrgmz <rzrgmz@outlook.com>
2024-05-17 20:22:20 +0800

    Site updated: 2024-05-17 20:22:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/17/hello-world/","2024/05/17/nginx/","2024/05/17/nginx-一篇通/"];function toRandomPost(){
+var posts=["2024/05/17/hello-world/","2024/05/17/nginx-一篇通/","2024/05/17/nginx-1/","2024/05/17/nginx/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
