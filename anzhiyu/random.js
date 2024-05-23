e536dfe67b43bdd106caa48a1a6d7f5242dc1405
rzrgmz <rzrgmz@outlook.com>
2024-05-23 16:19:35 +0800

    Site updated: 2024-05-23 16:19:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/17/nginx-一篇通/","2024/05/17/VPS购买/"];function toRandomPost(){
+var posts=["2024/05/17/VPS购买/","2024/05/17/nginx-一篇通/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
