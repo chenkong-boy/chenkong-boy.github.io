c922cecbf27392dad0531312eb670b3863a12585
rzrgmz <rzrgmz@outlook.com>
2024-05-23 18:43:43 +0800

    Site updated: 2024-05-23 18:43:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/17/VPS购买/","2024/05/17/nginx-一篇通/"];function toRandomPost(){
+var posts=["2024/05/17/VPS购买/","2024/05/17/nginx/nginx-基础/","2024/05/17/nginx/nginx-一篇通/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
