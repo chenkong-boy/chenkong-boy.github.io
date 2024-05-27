f9f7b5b5e0e89d549e113a4d6fd0ce51d9bc0d9b
rzrgmz <rzrgmz@outlook.com>
2024-05-27 20:18:18 +0800

    Site updated: 2024-05-27 20:18:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/17/VPS购买/","2024/05/17/hexo/HEXO-anzhiyu配置/","2024/05/17/K8S/K8S网络/","2024/05/17/hexo/HEXO各个大佬的配置网址/","2024/05/17/CICD/大纲/","2024/05/17/nginx/nginx-基础/","2024/05/17/nginx/nginx-一篇通/"];function toRandomPost(){
+var posts=["2024/05/17/VPS购买/","2024/05/17/hexo/HEXO-anzhiyu配置/","2024/05/17/K8S/K8S网络/","2024/05/17/CICD/大纲/","2024/05/17/hexo/HEXO各个大佬的配置网址/","2024/05/17/nginx/nginx-基础/","2024/05/17/nginx/nginx-一篇通/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
